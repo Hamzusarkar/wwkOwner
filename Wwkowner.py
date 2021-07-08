64d6069ddc8b26a36e77273cf6496c8976a4347c
Hamzusarkar <87085625+Hamzusarkar@users.noreply.github.com>
2021-07-08 12:00:23 +0000

    Create Wwkowner.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pkg install python2
    pip2 intsall requests
    pip2 install mechanize
    git clone
    https://github.com/Hamzusarkar/wwkOwner.git
    ls
    cd wwkOwner
    ls
    python2 Wwkowner.py

---
+++ Wwkowner.py
@@ -0,0 +1,229 @@
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To Wwk
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2020
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(10000):
+
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr)
+
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install mechanize')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install request')
+    time.sleep(1)
+    os.system('Then type: python2 Somi,')
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+	print 'Thanks.'
+	os.sys.exit()
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\33[%s;1m'%str(31+j))
+    x += '\33[0m'
+    x = x.replace('!0','\33[0m')
+    sys.stdout.write(x+'\')
+
+
+def jalan(z):
+	for e in z + '\':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\\1b[1;93mPlease Wait \1b[1;91m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = "\33[31mNot Vuln"
+vuln = "\33[32mVuln"
+
+os.system("clear")
+print  """
+\033[1;91m   ██              ██ ██               ██ ██   ██ 
+\033[1;91m   ██              ██ ██               ██ ██  ██
+\033[1;91m   █       █    ██ ██     █    ██ █████
+\033[1;91m   ██ ███ ██ ██   ███ ██ ██ ██ 
+\033[1;91m    ███ ███   ███ ███  ██   ██ 
+                            
+                            
+"""
+####Logo####
+
+logo1 = """
+\033[1;91m   ██              ██ ██               ██ ██   ██ 
+\033[1;91m   ██              ██ ██               ██ ██  ██
+\033[1;91m   █       █    ██ ██     █    ██ █████
+\033[1;91m   ██ ███ ██ ██   ███ ██ ██ ██ 
+\033[1;91m    ███ ███   ███ ███  ██   ██ 
+                            
+                            
+
+
+╒══════════════════WWK══════════════════╕
+\33[1;97m
+    NAM KISI OR KA KAM MA DALNA K SHOK 
+    YA TRI ZINDAGI KHREED K BHONK
+ \33[1;91m   DONT'COPY
+ \33[1;94mDEV•••Hamzu
+ \33[1;95m WHATAAP••••03416150256
+╘══════════════════brand══════════════════╛
+
+
+"""
+logo2 = """
+\33[1;97m
+                           _(\/) 
+
+                         ,((((^`\
+                        ((((  (6 \
+
+                      ,((((( ,    \
+  ,,,_              ,(((((  /"._  ,`,
+
+ ((((\ ,...       ,((((   /    `-.-'
+
+ )))  ;'    `"'"'""((((   (      
+
+(((  /            (((      \
+ )) |                      |
+
+((  |        .       '     |
+
+))  \    _ '      `t   ,.')
+                               \33[1;97mOnly WWK brand
+(   |   y;- -,-""'"-.\  \  
+
+)   / ./  ) /         `\ \
+   |./   ( (           / /'
+
+   ||     \          //'|
+
+   ||      \       _//'||
+
+   ||       ))     |_/  ||
+
+   \\    |_/          ||
+
+   `'"                  \\
+                        `'" 
+\33[1;92m
+\33[1;93m
+\33[1;97m                  S A R K A R
+\33[1;97m
+\33[1;93m
+\33[1;92m
+"""
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\\1b[1;93mPlease Wait \1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+cpb = []
+listgrup = []
+gagal = []
+idfriends = []
+idfromfriends = []
+idmem = []
+em = []
+emfromfriends = []
+hp = []
+hpfromfriends = []
+reaksi = []
+reaksigrup = []
+komen = []
+komengrup = []
+listgrup = []
+vulnot = "\33[31mNot Vuln"
+vuln = "\33[32mVuln"
+back = 0
+threads = []
+sucessful = []
+checkpoint = []
+oks = []
+action_failed = []
+idfriends = []
+idfromfriends = []
+member_id = []
+email= []
+number = []
+id = []
+em = []
+email_from_friends = []
+hp = []
+hpfromfriends = []
+reaction = []
+reactiongroup = []
+comment = []
+group_comment = []
+listgroup = []
+vulnot = "\33[31mNot Vuln"
+vuln = "\33[32mVuln"
+
+os.system("clear")
+print """
+\033[1;91m   ██              ██ ██               ██ ██   ██ 
+\033[1;91m   ██              ██ ██               ██ ██  ██
+\033[1;91m   █       █    ██ ██     █    ██ █████
+\033[1;91m   ██ ███ ██ ██   ███ ██ ██ ██ 
+\033[1;91m    ███ ███   ███ ███  ██   ██ 
+                            
+                            
+"'"
+jalan("\33[1;93m ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇")
+jalan("\33[1;93m▇▇\33[1;91m   WellCome to My new tools     \33[1;93m▇▇")
+jalan("\33[1;93m▇▇\33[1;92m              HAMZU BAD BOY        \33[1;93m▇▇")
+jalan("\33[1;93m▇▇\33[1;93m⚡𝐏𝐀𝐊𝐈𝐒𝐓𝐀𝐍 𝐢𝐬 𝐏𝐨𝐰𝐞𝐫 𝐨𝐟 𝐖𝐎𝐑𝐋𝐃⚡\33[1;93m▇▇")
+jalan("\33[1;93m▇▇\33[1;94m          Don't copy my scrpit........  \33[1;93m▇▇")
+jalan("\33[1;93m▇▇\33[1;95m                 Tool Using Tips     \33[1;93m▇▇")
+jalan("\33[1;93m▇▇\33[1;96m
